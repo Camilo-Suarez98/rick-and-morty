0cb38d7037968ee104f0d282339dc1b138fa6a79
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2025-11-08 15:39:35 -0500

    deat: include component for status of characters

---
+++ app/components/StatusBadge.tsx
@@ -0,0 +1,19 @@
+import { StatusBadgeProps } from "../types";
+
+export const StatusBadge = ({ status }: StatusBadgeProps) => {
+  const statusColors = {
+    Alive: 'bg-green-500/20 text-green-500 border-green-500/20',
+    Dead: 'bg-red-500/20 text-red-500 border-red-500/20',
+    unknown: 'bg-gray-500/20 text-gray-500 border-gray-500/20'
+  };
+
+  return (
+    <span
+      className={`text-base rounded-full px-2 py-1 ${statusColors[status as keyof typeof statusColors]}`}
+      role="status"
+      aria-label={status}
+    >
+      {status}
+    </span>
+  );
+};
