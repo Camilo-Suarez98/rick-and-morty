c41ed3e878ed98ef8038bfa0524a42f75b33e586
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2025-11-09 08:18:18 -0500

    feat: new component for add to favorites button

---
+++ app/components/AddToFavoriteButton.tsx
@@ -0,0 +1,32 @@
+import { Heart } from "lucide-react";
+import { Character } from "../types";
+
+type AddToFavoriteButtonProps = {
+  onToggleFavorite: (id: number) => void;
+  isFavorite: boolean;
+  character: Character;
+  size?: number;
+}
+
+export const AddToFavoriteButton = ({
+  onToggleFavorite,
+  isFavorite,
+  character,
+  size = 18
+}: AddToFavoriteButtonProps) => {
+  return (
+    <button
+      onClick={(e) => {
+        e.stopPropagation();
+        onToggleFavorite(character.id);
+      }}
+      className="absolute top-3 right-3 p-2 bg-slate-800 cursor-pointer backdrop-blur-sm rounded-full transition-colors hover:bg-cyan-500 hover:text-white"
+      aria-label={isFavorite ? 'Remove from favorites' : 'Add to favorites'}
+    >
+      <Heart
+        size={size}
+        className={isFavorite ? 'fill-red-500 text-red-500' : 'text-white'}
+      />
+    </button>
+  );
+};
