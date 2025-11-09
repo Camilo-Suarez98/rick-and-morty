700b2ede9374d811610772549705a82a7b76e0f3
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2025-11-09 14:40:40 -0500

    feat: include filters and search params

--- app/page.tsx
+++ app/page.tsx
@@ -4,51 +4,64 @@ import { useEffect, useState } from "react";
 import { Character } from "./types";
 import { CharacterCard } from "./components/CharacterCard";
 import { useRouter } from "next/navigation";
+import { useFavorites } from "./lib/hooks";
+import { Pagination } from "./components/Pagination";
+import { useDebounce } from "./lib/hooks";
+import { Filter } from "./components/Filters";
 
 export default function Home() {
-  const [characters, setCharacters] = useState<Character[]>([]);
-  const [isFavorite, setIsFavorite] = useState<boolean>(false);
   const router = useRouter();
+  const { favorites, toggleFavorite } = useFavorites();
+  const [characters, setCharacters] = useState<Character[]>([]);
+  const [filters, setFilters] = useState({
+    status: '',
+    gender: '',
+  });
+  const [searchTerm, setSearchTerm] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const debounceSearch = useDebounce(searchTerm, 300);
 
   useEffect(() => {
     const characters = async () => {
-      const data = await getCharacters("?page=1");
+      const params = new URLSearchParams({
+        page: currentPage.toString(),
+        ...(debounceSearch && { name: debounceSearch }),
+        ...(filters.status && { status: filters.status }),
+        ...(filters.gender && { gender: filters.gender }),
+      });
+      const data = await getCharacters(params);
       setCharacters(data.results);
+      setTotalPages(data.info.pages);
     };
     characters();
-  }, []);
-
-  const onToggleFavorite = (id: number) => {
-    setIsFavorite(!isFavorite);
-    const favoriteCharacters = localStorage.getItem("favoriteCharacters");
-    if (favoriteCharacters) {
-      const favoriteCharactersArray = JSON.parse(favoriteCharacters);
-      if (favoriteCharactersArray.includes(id)) {
-        localStorage.setItem("favoriteCharacters", JSON.stringify(favoriteCharactersArray.filter((item: number) => item !== id)));
-      } else {
-        localStorage.setItem("favoriteCharacters", JSON.stringify([...favoriteCharactersArray, id]));
-      }
-    } else {
-      localStorage.setItem("favoriteCharacters", JSON.stringify([id]));
-    }
-  };
+  }, [debounceSearch, filters, currentPage]);
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full flex-col items-center justify-between p-8 bg-white dark:bg-black">
-        <h1 className="text-3xl font-bold text-transparent mb-8 bg-clip-text bg-linear-to-r from-cyan-400 to-blue-500">Rick and Morty</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+    <div className="max-w-7xl mx-auto min-h-screen flex items-center justify-center font-sans">
+      <main className="flex min-h-screen w-full flex-col items-center justify-between p-4">
+        <Filter
+          filters={filters}
+          setFilters={setFilters}
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+        />
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
           {characters.map((character) => (
             <CharacterCard
               key={character.id}
               character={character}
-              isFavorite={isFavorite}
-              onToggleFavorite={() => onToggleFavorite(character.id)}
+              isFavorite={favorites.includes(character.id)}
+              onToggleFavorite={() => toggleFavorite(character.id)}
               onClick={() => { router.push(`/character/${character.id}`) }}
             />
           ))}
         </div>
-        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" onClick={() => getCharacters("?page=2")}>Next</button>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={setCurrentPage}
+        />
       </main>
     </div>
   );
